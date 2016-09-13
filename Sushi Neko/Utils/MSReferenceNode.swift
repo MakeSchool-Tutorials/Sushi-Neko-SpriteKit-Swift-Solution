3215b98c18aad5747be4ebdb21f353da323dbed6
Dion Larson <me@dionlarson.com>
2016-09-12 19:54:42 -0700

    Style changes for Swift 3

--- Sushi Neko/Utils/MSReferenceNode.swift
+++ Sushi Neko/Utils/MSReferenceNode.swift
@@ -13,8 +13,15 @@ class MSReferenceNode: SKReferenceNode {
     /* Avatar node connection */
     var avatar: SKSpriteNode!
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(fileNamed fileName: String?) {
+        super.init(fileNamed: fileName)
+    }
+    
     override func didLoad(_ node: SKNode?) {
-        
         /* Set reference to avatar node */
         avatar = childNode(withName: "//avatar") as! SKSpriteNode
     }
